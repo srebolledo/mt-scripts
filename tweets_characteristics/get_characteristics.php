8fb2ee0a1fbcdd7c75b0c48789f707f8d67b6f92
unknown <srebolledo@CSCLSREBOLLEDO.office.comscore.com>
2012-10-19 16:07:26 -0300

    added part of conversation check
    Signed-off-by: unknown <srebolledo@CSCLSREBOLLEDO.office.comscore.com>

--- tweets_characteristics/get_characteristics.php
+++ tweets_characteristics/get_characteristics.php
@@ -9,9 +9,10 @@
     $count_tweet_mentions = preg_match_all('/@.*/', $tweet, $matches);
     $count_tweet_hash = preg_match_all('/#.*/', $tweet, $matches);
     $count_url = preg_match_all('/http:\/\/.*/', $tweet, $matches);
+    $part_of_conversation = $row["tweet_reference"]!= 0 ? "1":"0" ;
     $id = $row['id'];
     //echo $tweet." (".$count_word."), (".$count_tweet_mentions."), ($count_url), ($count_tweet_hash)\n";
-    $sql = "Insert into tweet_characteristics (count_words, count_tweet_mentions, count_tweet_hash) values ($count_words, $count_tweet_mentions, $count_tweet_hash)";
+    $sql = "Insert into tweet_characteristics (count_words, count_tweet_mentions, count_tweet_hash, part_of_conversation) values ($count_words, $count_tweet_mentions, $count_tweet_hash, $part_of_conversation)";
     echo $sql."\n";
 
   }
