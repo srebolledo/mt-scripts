a5476eb64af6bf51f09569fcc8e0e515f6f0d56f
Stephan Rebolledo <srebolledo@gmail.com>
2012-11-22 16:37:57 -0300

    changes in the getter info part. if there's an error in the json getter, quit and wait.

--- tweets_characteristics/tweet_user_characteristics.php
+++ tweets_characteristics/tweet_user_characteristics.php
@@ -2,7 +2,7 @@
   require_once '../general/connection.php';
   mysql_select_db("twitter");
   while(1){
-    $sql = "select * from tweet_user_characteristics where issued = 0 limit 180";
+    $sql = "select * from tweet_user_characteristics where issued = 0 limit 150";
     $handler = mysql_query($sql) or die(mysql_error());
     $ids = array();
     $rows = array();
@@ -17,7 +17,9 @@
       $tweet_user_id = $row['id'];
       $json_url = "http://api.twitter.com/1/statuses/user_timeline.json?include_rts=true&screen_name=".$row['name']."&count=1";
       echo $json_url."\n";
-      $jsonObject = json_decode(file_get_contents($json_url),true);
+	  $json_to_parse = file_get_contents($json_url);
+	  if(!$json_to_parse) break;
+      $jsonObject = json_decode($json_to_parse,true);
       print_r($jsonObject[0]['user']);
       $userObject = $jsonObject[0]['user'];
       $followers = $userObject['followers_count'];
@@ -29,7 +31,8 @@
       echo $sql."\n";
       mysql_query($sql);
     }
-    sleep(905);
+	echo "\n\n***************************\n\nREACHED LIMIT!\n\n***************************\n\n";
+    sleep(300);
     $sql = "update tweet_user_characteristics set issued = 0 where number_of_tweets = 0";
     mysql_query($sql);
   }
