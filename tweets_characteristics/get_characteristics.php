ced66dcd3e198e1dd5e3df83869adf49dd202dd4
Stephan Rebolledo <srebolledo@gmail.com>
2012-12-03 23:12:36 -0300

    Added tweet_reference in the get_characteristics script

--- tweets_characteristics/get_characteristics.php
+++ tweets_characteristics/get_characteristics.php
@@ -2,7 +2,7 @@
   require_once '../general/connection.php';
   mysql_select_db("twitter");
   mysql_query("truncate table tweet_characteristics");
-  $sql = "select id, tweet from tweets_new";
+  $sql = "select id, tweet, tweet_reference from tweets_new";
   $handler = mysql_query($sql) or die(mysql_error());
   while($row = mysql_fetch_array($handler)){
     $tweet = trim($row['tweet']);
@@ -13,7 +13,6 @@
     $count_url = preg_match_all('/http:\/\/.*/', $tweet, $matches);
     $part_of_conversation = $row["tweet_reference"]!= 0 ? "1":"0" ;
     $id = $row['id'];
-    //echo $tweet." (".$count_word."), (".$count_tweet_mentions."), ($count_url), ($count_tweet_hash)\n";
     $sql = "Insert into tweet_characteristics (count_words, count_tweet_mentions, count_tweet_hash, part_of_conversation) values ($count_words, $count_tweet_mentions, $count_tweet_hash, $part_of_conversation)";
     echo $sql."\n";
 
