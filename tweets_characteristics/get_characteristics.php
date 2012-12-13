31bcc1d30cdd18367082215a28a9e0ec17355d03
Stephan Rebolledo <srebolledo@gmail.com>
2012-12-12 21:44:54 -0300

    fixing get_characteristics

--- tweets_characteristics/get_characteristics.php
+++ tweets_characteristics/get_characteristics.php
@@ -2,7 +2,7 @@
   require_once '../general/connection.php';
   mysql_select_db("twitter");
   mysql_query("truncate table tweet_characteristics");
-  $sql = "select id, tweet, tweet_reference from tweets_new";
+  $sql = "select id, tweet, tweet_reference from tweets_new limit 1";
   $handler = mysql_query($sql) or die(mysql_error());
   while($row = mysql_fetch_array($handler)){
     $tweet = trim($row['tweet']);
@@ -13,7 +13,7 @@
     $count_url = preg_match_all('/http:\/\/.*/', $tweet, $matches);
     $part_of_conversation = $row["tweet_reference"]!= 0 ? "1":"0" ;
     $id = $row['id'];
-    $sql = "Insert into tweet_characteristics (count_words, count_tweet_mentions, count_tweet_hash, part_of_conversation) values ($count_words, $count_tweet_mentions, $count_tweet_hash, $part_of_conversation)";
+    $sql = "Insert into tweet_characteristics values (null, $id, $count_words,0,0,0, $count_tweet_mentions, $count_tweet_hash, $part_of_conversation)";
     echo $sql."\n";
 
   }
